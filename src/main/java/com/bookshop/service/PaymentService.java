e7f0d96f3ea70ce3fde6e23da1a0e8a100996b7b
jieun <heyfer6867@gmail.com>
2025-09-02 15:07:51 +0900

    refactor: 필요없는 주석 삭제

--- src/main/java/com/bookshop/service/PaymentService.java
+++ src/main/java/com/bookshop/service/PaymentService.java
@@ -1,22 +1,12 @@
 package com.bookshop.service;
 
 import com.bookshop.domain.Payment;
-import com.bookshop.dto.PaymentVerificationResult;
 import com.bookshop.repository.PaymentRepository;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
-
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+
 
 //아임포트 API와 통신하여 결제를 검증하는 서비스
 @Service
